1d91e36ec2f0e2c9c46d91c5c9485f680a298e14
NgnDinhThanh <thanh.nd200592@sis.hust.edu.vn>
2024-05-21 17:24:06 +0700

    Build Header UI #2

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,26 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTApPHwoywrFHUu_6HkTQt9RSFayC-3UCHibD5dUU9_OA&s"
+                alt="Hoa"
+            />
+            <h4 className={cx('info')}>
+                <p className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </p>
+                <span className={cx('username')}>nguyenvana</span>
+            </h4>
+        </div>
+    );
+}
+
+export default AccountItem;
